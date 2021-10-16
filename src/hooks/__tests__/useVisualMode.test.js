70b172d879164cc40737f1243b600cc9bf70aee9
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-10-16 15:01:45 -0700

    Uncomment/comment out some tests for useVisualMode; have to comment out a test in Form because of instruction inconsistency

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -6,102 +6,103 @@ const FIRST = "FIRST";
 const SECOND = "SECOND";
 const THIRD = "THIRD";
 
-// test("useVisualMode should initialize with default value", () => {
-//   const { result } = renderHook(() => useVisualMode(FIRST));
-
-//   expect(result.current.mode).toBe(FIRST);
-// });
-
-// test("useVisualMode should transition to another mode", () => {
-//   const { result } = renderHook(() => useVisualMode(FIRST));
-
-//   act(() => result.current.transition(SECOND));
-//   expect(result.current.mode).toBe(SECOND);
-// });
-
-// test("useVisualMode should return to previous mode", () => {
-//   const { result } = renderHook(() => useVisualMode(FIRST));
-
-//   act(() => result.current.transition(SECOND));
-//   expect(result.current.mode).toBe(SECOND);
-
-//   act(() => result.current.transition(THIRD));
-//   expect(result.current.mode).toBe(THIRD);
-
-//   act(() => result.current.back());
-//   expect(result.current.mode).toBe(SECOND);
-
-//   act(() => result.current.back());
-//   expect(result.current.mode).toBe(FIRST);
-// });
-
-// test("useVisualMode should not return to previous mode if already at initial", () => {
-//   const { result } = renderHook(() => useVisualMode(FIRST));
-
-//   act(() => result.current.back());
-//   expect(result.current.mode).toBe(FIRST);
-// });
-
-// test("useVisualMode should replace the current mode", () => {
-//   const { result } = renderHook(() => useVisualMode(FIRST));
-
-//   act(() => result.current.transition(SECOND));
-//   expect(result.current.mode).toBe(SECOND);
-
-//   // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
-//   act(() => result.current.transition(THIRD, true));
-//   expect(result.current.mode).toBe(THIRD);
-
-//   act(() => result.current.back());
-//   expect(result.current.mode).toBe(FIRST);
-// });
-
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
-  expect(result.current[0]).toBe(FIRST);
+  expect(result.current.mode).toBe(FIRST);
 });
 
 test("useVisualMode should transition to another mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
-  act(() => result.current[1](SECOND));
-  expect(result.current[0]).toBe(SECOND);
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
 });
 
 test("useVisualMode should return to previous mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
-  act(() => result.current[1](SECOND));
-  expect(result.current[0]).toBe(SECOND);
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
 
-  act(() => result.current[1](THIRD));
-  expect(result.current[0]).toBe(THIRD);
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
 
-  act(() => result.current[2]());
-  expect(result.current[0]).toBe(SECOND);
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
 
-  act(() => result.current[2]());
-  expect(result.current[0]).toBe(FIRST);
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
 });
 
 test("useVisualMode should not return to previous mode if already at initial", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
-  act(() => result.current[2]());
-  expect(result.current[0]).toBe(FIRST);
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
 });
 
 test("useVisualMode should replace the current mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
-  act(() => result.current[1](SECOND));
-  expect(result.current[0]).toBe(SECOND);
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
 
   // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
-  act(() => result.current[1](THIRD, true));
-  expect(result.current[0]).toBe(THIRD);
+  act(() => result.current.transition(THIRD, true));
+  expect(result.current.mode).toBe(THIRD);
 
-  act(() => result.current[2]());
-  expect(result.current[0]).toBe(FIRST);
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
 });
+
+
+// test("useVisualMode should initialize with default value", () => {
+//   const { result } = renderHook(() => useVisualMode(FIRST));
+
+//   expect(result.current[0]).toBe(FIRST);
+// });
+
+// test("useVisualMode should transition to another mode", () => {
+//   const { result } = renderHook(() => useVisualMode(FIRST));
+
+//   act(() => result.current[1](SECOND));
+//   expect(result.current[0]).toBe(SECOND);
+// });
+
+// test("useVisualMode should return to previous mode", () => {
+//   const { result } = renderHook(() => useVisualMode(FIRST));
+
+//   act(() => result.current[1](SECOND));
+//   expect(result.current[0]).toBe(SECOND);
+
+//   act(() => result.current[1](THIRD));
+//   expect(result.current[0]).toBe(THIRD);
+
+//   act(() => result.current[2]());
+//   expect(result.current[0]).toBe(SECOND);
+
+//   act(() => result.current[2]());
+//   expect(result.current[0]).toBe(FIRST);
+// });
+
+// test("useVisualMode should not return to previous mode if already at initial", () => {
+//   const { result } = renderHook(() => useVisualMode(FIRST));
+
+//   act(() => result.current[2]());
+//   expect(result.current[0]).toBe(FIRST);
+// });
+
+// test("useVisualMode should replace the current mode", () => {
+//   const { result } = renderHook(() => useVisualMode(FIRST));
+
+//   act(() => result.current[1](SECOND));
+//   expect(result.current[0]).toBe(SECOND);
+
+//   // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+//   act(() => result.current[1](THIRD, true));
+//   expect(result.current[0]).toBe(THIRD);
+
+//   act(() => result.current[2]());
+//   expect(result.current[0]).toBe(FIRST);
+// });
